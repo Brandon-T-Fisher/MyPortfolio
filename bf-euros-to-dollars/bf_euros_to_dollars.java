63ff9e0d62b995f9560743b3752177e720179bcd
Fisher <BrandonTFisher05@gmail.com>
2021-11-30 13:05:34 -0500

    Added a euro converter and bmi converter.

---
+++ bf-euros-to-dollars/bf_euros_to_dollars.java
@@ -0,0 +1,24 @@
+ 
+import java.util.Scanner;
+
+/**
+ * Write a description of class EurosToDolllars here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class bf_euros_to_dollars
+{
+    public static void main(String[] args) {
+        //Create a Scanner Object so I can read in from the 
+        // command line.
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("How many dollars do you want to convert?");
+        double dollars = keyboard.nextDouble();
+        System.out.print("What is the euros-per-dollar exchange rate?");
+        double rate = keyboard.nextDouble();
+        double result = dollars*rate;
+        System.out.printf("%.2f dollars => %.2f ", dollars,result);
+        
+    }
+}

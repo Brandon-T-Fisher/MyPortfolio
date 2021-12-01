e9dc6b1c11db4a8f62601ba739dea3a91bcd44ef
Fisher <BrandonTFisher05@gmail.com>
2021-12-01 11:43:09 -0500

    added a drawing project and updated rising sun project

--- RisingSun/RisingSun.java
+++ RisingSun/RisingSun.java
@@ -1,37 +1,39 @@
+package RisingSun;
+
 import objectdraw.*;
 import java.awt.*;
 
 /**
  * Write a description of class RisingSun here.
- *
- * @author (your name)
+ * 
+ * @author (your name) 
  * @version (a version number or a date)
  */
 public class RisingSun extends WindowController
 {
     
-    private FilledOval sun;
+    private FilledOval sun;   
     private Text instructions;
+    private Location initialLocation = new Location(150,350);
     
     // instance variables - replace the example below with your own
-   public void begin() {
-      sun = new FilledOval(150,350,100,100,canvas);
-
-      instructions = new Text("Please click the mouse repeatedly",20,20,canvas);
-      
-      
+   public void begin() 
+   {
+       sun = new FilledOval(initialLocation,100,100,canvas);
+       Color myColor = new Color(0,200,255);
+       sun.setColor(myColor);
+       instructions = new Text("Please click on the mouse repeatedly", 20, 20, canvas);
    }
    
    public void onMouseClick(Location point)
-{
-    sun.move(25, 0);
-    instructions.hide();
-}
+   {
+       sun.move(0,-10);
+       instructions.hide();
+   }
 
-public void onMouseExit(Location point)
-{
-    sun.moveTo(150, 350);
-    instructions.show();
-
-}
+   public void onMouseExit(Location point)
+   {
+       sun.moveTo(initialLocation);
+       instructions.show();
+   }
 }
